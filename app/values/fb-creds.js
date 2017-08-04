c8400993d988204c1964a83a79553cd7577f4cd1
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-04 01:57:08 -0500

    setup index and link scripts and header info

--- app/values/fb-creds.js
+++ app/values/fb-creds.js
@@ -1,6 +1,6 @@
 'use strict';
 
-todoApp.constant("FBCreds", {
-  key: "3e58ec693ab698ba8d25a4aec1f14631",
-  authDomain:"todo-app-19fec.firebaseapp.com"
+myApp.constant("FBCreds", {
+  key: "AIzaSyDz9jfYLhzXLsPD6Ej823mgdBaK1vIXMYo",
+  authDomain: "bobbys-frontend-capstone.firebaseapp.com"
 });
