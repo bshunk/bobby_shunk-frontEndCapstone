0d4a25b3cac77ed9bcbd33fbb9587838619d5125
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-07 09:27:59 -0500

    resolve grunt errors in appJS and add searched file data from APIs to data file

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 'use strict';
+// global.mashapeKey = '3e58ec693ab698ba8d25a4aec1f14631';
+// const client = igdb('3e58ec693ab698ba8d25a4aec1f14631');
+// console.log(client);
 
 let myApp = angular.module("MyApp", ["ngRoute"])
 .constant("FirebaseUrl", "https://bobbys-frontend-capstone.firebaseio.com");
@@ -15,36 +18,16 @@ let isAuth = (UserFactory) => {
   });
 };
 
-import igdb from 'igdb-api-node';
+// client.games({
+//     fields: '*', // Return all fields
+//     limit: 5, // Limit to 5 results
+//     offset: 15 // Index offset for results
+// }).then(response => {
+//     // response.body contains the parsed JSON response to this query
+// }).catch(error => {
+//     throw error;
+// });
+// console.log("client", client);
 
-const client = igdb('3e58ec693ab698ba8d25a4aec1f14631');
-
-client.games({
-    fields: '*', // Return all fields
-    limit: 5, // Limit to 5 results
-    offset: 15 // Index offset for results
-}).then(response => {
-    // response.body contains the parsed JSON response to this query
-}).catch(error => {
-    throw error;
-});
-console.log("client", client);
-
-// "use strict";
-
-// let myApp = angular.module("MyApp", ["ngRoute"])
-// .constant("FirebaseUrl", "https://api-2445582011268.apicast.io/");
-
-// let isAuth = (UserFactory) => {
-//   return new Promise( (resolve, reject) => {
-//     UserFactory.isAuthenticated()
-//     .then( (userBoolean) => {
-//       if(userBoolean) 
-//         resolve(); 
-//       else
-//         reject();
-//     });
-//   });
-// };
 
 
