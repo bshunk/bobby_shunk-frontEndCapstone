b697a9abc90b8a82cdc13753a3d85bc9c4cb2154
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-07 11:56:26 -0500

    work on authentication

--- app/factories/user-factory.js
+++ app/factories/user-factory.js
@@ -1,68 +1,72 @@
-// 'use strict';
+'use strict';
 
-// myApp.factory("UserFactory", function($q, $http, FirebaseUrl, FBCreds) {
+// setting myApp.factory to equal UserFactory within partial with the anonymous function called expecting $q, $http, FirebaseUrl and FBCreds to be passed in at some point
+myApp.factory("UserFactory", function($q, $http, FirebaseUrl, FBCreds) {
+  
+  // setting config to equal the object containing the firebase key and auth domain
+  var config = {
+    apiKey: FBCreds.key,
+    authDomain: FBCreds.authDomain
+  };
 
-//   var config = {
-//     apiKey: FBCreds.key,
-//     authDomain: FBCreds.authDomain
-//   };
+  // passes config into initializeApp within firebase
+  firebase.initializeApp(config);
 
-//   firebase.initializeApp(config);
+  // setting current user to equal null because so when no one is logged in a profile won't display
+  let currentUser = null;
+  console.log("this is what current user equals", currentUser);
 
-//   let currentUser = null;
+  let isAuthenticated = function() {
+    console.log("isAuthenticated called");
+    return new Promise( (resolve, reject) => {
+      console.log("firing onAuthStateChanged");
+      firebase.auth().onAuthStateChanged(function(user) {
+        console.log("onAuthStateChanged finished");
+        if (user) {
+          console.log("user", user);
+          currentUser = user.uid;
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  };
 
-//   let isAuthenticated = function() {
-//     console.log("isAuthenticated called");
-//     return new Promise( (resolve, reject) => {
-//       console.log("firing onAuthStateChanged");
-//       firebase.auth().onAuthStateChanged(function(user) {
-//         console.log("onAuthStateChanged finished");
-//         if (user) {
-//           console.log("user", user);
-//           currentUser = user.uid;
-//           resolve(true);
-//         } else {
-//           resolve(false);
-//         }
-//       });
-//     });
-//   };
+  let getUser = () => {
+    return currentUser;
+  };
 
-//   let getUser = () => {
-//     return currentUser;
-//   };
+  let createUser = (userObj) => {
+    return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
+    .catch( (err) => {
+      console.log("error creating user", err.message);
+    });
+  };
 
-//   let createUser = (userObj) => {
-//     return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
-//     .catch( (err) => {
-//       console.log("error creating user", err.message);
-//     });
-//   };
+  let loginUser = (userObj) => {
+    return $q( (resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
+      .then( (user) => {
+        // have to set the current user here because the controllers that call `getUser` ( todo-controller, for example) are loading before the `onAuthStateChanged` listener was kicking in and setting the user value
+        currentUser = user.uid;
+        resolve(user);
+      })
+      .catch( (err) => {
+        console.log("error logging in", err.message);
+      });
+    });
+  };
 
-//   let loginUser = (userObj) => {
-//     return $q( (resolve, reject) => {
-//       firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
-//       .then( (user) => {
-//         // have to set the current user here because the controllers that call `getUser`
-//         // ( todo-controller, for example) are loading before the `onAuthStateChanged`
-//         // listener was kicking in and setting the user value
-//         currentUser = user.uid;
-//         resolve(user);
-//       })
-//       .catch( (err) => {
-//         console.log("error loggin in", err.message);
-//       });
-//     });
-//   };
+  let logoutUser = () => {
+    return firebase.auth().signOut()
+    .catch( (err) => {
+      console.log("error logging out", err.message);
+    });
+  };
 
-//   let logoutUser = () => {
-//     return firebase.auth().signOut()
-//     .catch( (err) => {
-//       console.log("error loggin' out, man", err.message);
-//     });
-//   };
+  console.log("firebase", firebase);
 
-//   console.log("firebase", firebase );
+  return {isAuthenticated, getUser, createUser, loginUser, logoutUser};
 
-//   return {isAuthenticated, getUser, createUser, loginUser, logoutUser};
-// });
+});
