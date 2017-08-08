5edccf44bcd6004d05677b16794423fa022f0964
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-08 10:06:12 -0500

    get authentication to work, log in and logout to display correctly

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -1,30 +1,28 @@
-// 'use strict';
+'use strict';
 
-// myApp.controller("UserController", function($scope, $window, UserFactory) {
+myApp.controller("UserController", function($scope, $window, UserFactory) {
 
-//   $scope.account = {
-//     email: "",
-//     password: ""
-//   };
+  $scope.account = {
+    email: "",
+    password: "",
+  };
 
-//   $scope.register = () => {
-//     // TODO validate that user doesn't exist
-//     console.log("you clicked register");
-//     UserFactory.createUser($scope.account)
-//     .then( (userData) => {
-//       console.log("New User!", userData);
-//       $scope.login();
-//     });
-//   };
+  $scope.register = () => {
+    // TODO validate that user doesn't exist so they'll be taken to login
+    console.log("you clicked register");
+    UserFactory.createUser($scope.account)
+    .then( (userData) => {
+      console.log("New User!", userData);
+      $scope.login();
+    });
+  };
 
-//   $scope.login = () => {
-//     UserFactory.loginUser($scope.account)
-//     .then( (userData) => {
-//       console.log("userData", userData);
-//       $window.location.href = '#!/todos/view';
-//     });
-//   };
-  
-// });
+  $scope.login = () => {
+    UserFactory.loginUser($scope.account)
+    .then( (userData) => {
+      console.log("user data", userData);
+      $window.location.href = '#!/games/home';
+    });
+  };
 
-
+});
